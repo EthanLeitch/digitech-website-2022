0e15a7875b6f99ec21dcfd66757757af3527bc8f
Ethan <ethanjohnleitch@gmail.com>
2022-05-11 10:17:06 +1200

    11 May commit

--- app.js
+++ app.js
@@ -8,16 +8,28 @@ const port = 3000;
 
 
 
-const server = express();
+const app = express();
 
-server.get("/", (req, res) => {
+
+
+/*app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'src/html/index.html'))
 });
 
 
 
+app.get('*', function(req, res){
+  // res.send('404 Page not found', 404);
+  res.sendFile(path.join(__dirname, 'src/html/404.html'))
+});*/
 
-server.listen(port, hostname, () => {
+app.use(express.static('src/html'));
+app.get('*', function(req, res){
+  // res.send('404 Page not found', 404);
+  res.sendFile(path.join(__dirname, 'src/html/404.html'))
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
   // console.log(module);
 });
