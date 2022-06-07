79040820154bfe299dd2f3073d6f0ac80c378822
Ethan <ethanjohnleitch@gmail.com>
2022-06-08 10:15:40 +1200

    8 June commit

--- src/javascript/theme.js
+++ src/javascript/theme.js
@@ -1,24 +1,32 @@
 const changeThemeToLight = () => {
+    checkbox.checked = false;
     document.documentElement.setAttribute("data-theme", "light")//set theme to light
-    localStorage.setItem("data-theme", 'light')
+    localStorage.setItem("data-theme", "light")
 }
+
 const changeThemeToDark = () => {
+    checkbox.checked = true;
     document.documentElement.setAttribute("data-theme", "dark")//set theme to dark
     localStorage.setItem("data-theme", "dark")
 }
-changeThemeToDark();
 
 // Get the element based on ID
 const checkbox = document.getElementById("switch");
 let theme = localStorage.getItem('data-theme');
 
-// Apply retrived them to the website
+// Check theme preference and set it 
+if (theme == 'light'){
+    changeThemeToLight()
+} else {
+    changeThemeToDark()
+}
+
+// Listen for a change in the button, and update theme accordingly
 checkbox.addEventListener('change', () => {
-    //alert("ARGAIOSFDVSERNGIVN");
     let theme = localStorage.getItem('data-theme'); // Retrieve saved them from local storage
-    if (theme ==='dark'){
+    if (theme == 'dark'){
         changeThemeToLight()
-    }else{
+    } else {
         changeThemeToDark()
-    }   
+    }
 });
