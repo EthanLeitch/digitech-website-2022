db9ded8287a25dd15d94ad5dd81c4b60c2bbdace
Ethan <ethanjohnleitch@gmail.com>
2022-07-06 10:16:43 +1200

    6 July commit - started database linking

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # create the application object
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/classroom_db'
+
+db = SQLAlchemy(app)
+
+# Create database model
+class classrooms(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_number = db.Column(db.Integer, nullable=False)
+    latitude = db.Column(db.DECIMAL(8, 6))
+    longitude = db.Column(db.DECIMAL(9, 6))
+
+    def __repr__(self):
+        return '<User %r>' % self.room_number
+
 
 # use decorators to link the function to a url
 @app.route('/')
