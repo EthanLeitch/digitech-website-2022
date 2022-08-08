ce28cc290fad4f8b7fde614e69fca834c9c85e26
Ethan <ethanjohnleitch@gmail.com>
2022-08-08 15:11:12 +1200

    8 August commit - List filtering working partially

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -22,6 +22,39 @@ marker.bindPopup("<b>John McGlashan College</b><br>I am a popup."); */
 
 var gpsMarker = null;
 
+function searchFunction() {
+	// Declare variables
+	var input, filter, ul, li, a, i, txtValue;
+	input = document.getElementById('myInput');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("myUL");
+	li = ul.getElementsByTagName('li');
+
+	if(input.value.length < 3) {
+    	for (i = 0; i < li.length; i++) {
+        	li[i].style.display = "";
+		}
+        return;
+	}
+
+	// Loop through all list items, and hide those who don't match the search query
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("a")[0];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
+}
+
+function toggleList() {
+	for (i = 0; i < li.length; i++) {
+		li[i].style.display = "none";
+	}
+}
+
 $.getJSON("/getpythondata", function(data) {
     // alert(JSON.stringify(data.classrooms));
 	var classroomData = data.classroom;
